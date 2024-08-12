d49d5fe088b19c2fb3c0fe8f3fc44715157622d5
Nasir-Ibn-e-Zahir <mnasir477447@gmail.com>
2024-08-12 14:03:21 +0500

    DIsplay Generas

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={10}
+        spacing={2}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
