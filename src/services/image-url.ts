2e110a02c13208ac904a54ab6ba421f93659f639
Nasir-Ibn-e-Zahir <mnasir477447@gmail.com>
2024-08-13 14:36:53 +0500

    Add empty image to !url

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
-
+import emptyImage from '../assets/empty.jpg'
 
 const getCroppedImageUrl = (url:string) => {
+    if(!url) return emptyImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
