5ab8305e2139e13e9efc82fd6e533325d6fe90dc
Nasir-Ibn-e-Zahir <mnasir477447@gmail.com>
2024-08-14 10:38:55 +0500

    Clean up Game Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import PlatFormIconsList from './PlatFormIconsList'
-import CrtictScore from './CrtictScore'
-import getCroppedImageUrl from '../services/image-url'
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import PlatFormIconsList from "./PlatFormIconsList";
+import CrtictScore from "./CrtictScore";
+import getCroppedImageUrl from "../services/image-url";
 
-interface Props{
-    game: Game
+interface Props {
+  game: Game;
 }
 
-const GameCard = ({game}:Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-  <>
-  <Card>
-    <Image src={getCroppedImageUrl(game.background_image)} />
-    <CardBody>   
-        <Heading fontSize='2xl' >
-            {game.name}
-        </Heading>
-        <HStack justifyContent='space-between' >
-          <PlatFormIconsList platforms={game.parent_platforms.map(p => p.platform)}  />
-          <CrtictScore  criticScore={game.metacritic} />  
-        </HStack>
-           </CardBody>
-  </Card>
-  </>
-  )
-}
+    <>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatFormIconsList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CrtictScore criticScore={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
 
-export default GameCard
+export default GameCard;
